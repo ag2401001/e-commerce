9ddcc0adc1d6359d14b73f43c6dde24a495a9347
Ayush Gupta <ag2401001@gmail.com>
2025-02-18 11:59:47 +0530

    include all static pages in for frontend

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/shop', function () {
+    return view('shop');
+})->name('shop');
+
+Route::get('/cart', function () {
+    return view('cart');
+})->name('cart');
+
+Route::get('/checkout', function () {
+    return view('checkout');
+})->name('checkout');
+
+Route::get('/testimonial', function () {
+    return view('testimonial');
+})->name('testimonial');
+
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+
+Route::get('/product-detail', function () {
+    return view('product-detail');
+})->name('product.detail');
